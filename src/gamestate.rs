63bdb439da43a936b49491999c71508d772c589a
Khang Tran <ktra@email.unc.edu>
2021-01-11 18:49:11 -0500

    Added start_game, Double, gamecoordinator logic and testing

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use std::option::Option;
 use uuid::Uuid;
 
-pub type ChipPile = i32;
+pub type ChipPile = f32;
 pub type Hand = Vec<i32>;
 pub type PlayerID = Uuid;
 
@@ -15,11 +15,12 @@ pub struct GameState {
     player_hand: HashMap<PlayerID, Hand>,
     player_money: HashMap<PlayerID, ChipPile>,
     player_bet: HashMap<PlayerID, ChipPile>,
+    player_round_over: Vec<PlayerID>,
     dealer_hand: Hand,
     deck: Vec<i32>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
 pub enum GameAction {
     Hit,
     Stand,
@@ -28,7 +29,7 @@ pub enum GameAction {
     StartingBet(ChipPile),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum GameError {
     MissingPlayerID,
     InvaildAction,
@@ -48,7 +49,7 @@ pub enum ClientEvent {
 }
 
 impl GameState {
-    pub fn new(users: &Vec<PlayerID>) -> Self {
+    pub fn new(users: Vec<PlayerID>) -> Self {
         Self {
             current_player: None,
             player_list: users.to_vec(),
@@ -56,14 +57,15 @@ impl GameState {
             player_money: HashMap::new(),
             player_bet: HashMap::new(),
             dealer_hand: Vec::new(),
-            deck: vec![2, 8, 11, 10, 10, 10, 10, 7],
+            player_round_over: Vec::new(),
+            deck: Vec::new(),
         }
     }
 
     pub fn create_users_hand(&mut self) {
         for id in &self.player_list {
             self.player_hand.insert(*id, Vec::new());
-            self.player_money.insert(*id, 0);
+            self.player_money.insert(*id, 0.0);
         }
     }
 
@@ -71,34 +73,66 @@ impl GameState {
         self.player_list.retain(|&x| x != player);
         self.player_hand.retain(|x, _| *x != player);
         self.player_money.retain(|x, _| *x != player);
+        self.player_bet.retain(|x, _| *x != player);
+        self.player_round_over.retain(|&x| x != player);
 
         player
     }
 
     pub fn start_game(&mut self) {
         if self.current_player == None {
+            // Deals cards in a staggered way
+            while self.dealer_hand.len() < 2 {
+                for player in self.player_list.clone() {
+                    self.current_player = Some(player);
+                    self.action(GameAction::Hit, player).ok();
+                }
+                self.dealer_draw();
+                if self.dealer_hand.len() == 1 {
+                    ClientEvent::CardRevealed(FromPlayer::Dealer, self.dealer_hand[0]);
+                }
+            }
             self.current_player = Some(self.player_list[0]);
-            // for player in self.player_list {
-            //     self.action(GameAction::Hit, player).ok();
-            // }
         }
     }
 
-    pub fn next_current_player(&mut self, player: PlayerID) -> Result<(), GameError> {
+    pub fn check_natural_blackjack(&mut self) -> Result<(), GameError> {
+        for player in self.player_list.clone() {
+            if self.sum_hand(player)? == 21 {
+                self.player_round_over.push(player);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn next_current_player(&mut self, player: PlayerID) -> Result<PlayerID, GameError> {
         let mut iter = self.player_list.iter();
         let _ = iter.by_ref().find(|&&id| player == id);
-        Ok(self.current_player = Some(*iter.next().ok_or(GameError::MissingPlayerID)?))
+        self.current_player = Some(*iter.next().ok_or(GameError::MissingPlayerID)?);
+        Ok(self.get_result_current_player()?)
     }
 
     pub fn get_current_player(&self) -> Option<PlayerID> {
         self.current_player
     }
 
+    pub fn get_result_current_player(&self) -> Result<PlayerID, GameError> {
+        self.current_player.ok_or(GameError::MissingPlayerID)
+    }
+
+    pub fn get_deck(&self) -> &Vec<i32> {
+        &self.deck
+    }
+
+    pub fn get_mut_deck(&mut self) -> &mut Vec<i32> {
+        &mut self.deck
+    }
+
     pub fn get_player_list(&self) -> &Vec<PlayerID> {
         &self.player_list
     }
 
-    pub fn get_dealer_hand(& self) -> &Hand {
+    pub fn get_dealer_hand(&self) -> &Hand {
         &self.dealer_hand
     }
 
@@ -114,46 +148,34 @@ impl GameState {
             .ok_or(GameError::MissingPlayerID)
     }
 
-    pub fn get_player_money(&self, player: PlayerID) -> Result<i32, GameError> {
+    pub fn get_player_money(&self, player: PlayerID) -> Result<f32, GameError> {
         self.player_money
             .get(&player)
             .ok_or(GameError::MissingPlayerID)
             .map(|money| *money)
     }
 
-    pub fn get_mut_player_money(&mut self, player: PlayerID) -> Result<&mut i32, GameError> {
+    pub fn get_mut_player_money(&mut self, player: PlayerID) -> Result<&mut f32, GameError> {
         self.player_money
             .get_mut(&player)
             .ok_or(GameError::MissingPlayerID)
     }
 
-    pub fn set_player_money(&mut self, player: PlayerID, value: i32) -> Result<(), GameError> {
-        *self
-            .player_money
-            .get_mut(&player)
-            .ok_or(GameError::MissingPlayerID)? = value;
-        Ok(())
-    }
-
-    pub fn get_player_bet(&self, player: PlayerID) -> Result<i32, GameError> {
+    pub fn get_player_bet(&self, player: PlayerID) -> Result<f32, GameError> {
         self.player_bet
             .get(&player)
             .ok_or(GameError::MissingPlayerID)
             .map(|bet| *bet)
     }
 
-    pub fn get_mut_player_bet(&mut self, player: PlayerID) -> Result<&mut i32, GameError> {
+    pub fn get_mut_player_bet(&mut self, player: PlayerID) -> Result<&mut f32, GameError> {
         self.player_bet
             .get_mut(&player)
             .ok_or(GameError::MissingPlayerID)
     }
 
-    pub fn set_player_bet(&mut self, player: PlayerID, value: i32) -> Result<(), GameError> {
-        *self
-            .player_bet
-            .get_mut(&player)
-            .ok_or(GameError::MissingPlayerID)? = value;
-        Ok(())
+    pub fn get_player_round_over(&self) -> &Vec<PlayerID> {
+        &self.player_round_over
     }
 
     pub fn is_round_over(&mut self, event: Vec<ClientEvent>) -> bool {
@@ -194,24 +216,24 @@ impl GameState {
     }
 
     pub fn dealer_draw_final(&mut self) {
-        let new_card = self.deck.remove(0);
         while self.sum_dealer() < 17 {
+            let new_card = self.deck.remove(0);
             self.dealer_hand.push(new_card);
+            ClientEvent::CardRevealed(FromPlayer::Dealer, new_card);
         }
     }
 
     pub fn compare_hands(&mut self) -> Result<(), GameError> {
-        // iter_mut, if bet value is 0 continue for loop
-        let mut clone_bet = self.player_bet.clone();
-        clone_bet.retain(|_, x| *x != 0);
-
         let dealer_sum = self.sum_dealer();
 
-        for key in clone_bet.keys() {
-            if dealer_sum <= 21 && self.sum_hand(*key)? < dealer_sum {
-                *self.get_mut_player_bet(*key)? *= 0;
+        for key in self.player_bet.clone().keys() {
+            let player_sum = self.sum_hand(*key)?;
+            if dealer_sum <= 21 && player_sum < dealer_sum || player_sum > 21 {
+                *self.get_mut_player_bet(*key)? *= 0.0;
+            } else if self.sum_hand(*key)? == 21 {
+                *self.get_mut_player_bet(*key)? *= 2.5;
             } else {
-                *self.get_mut_player_bet(*key)? *= 2;
+                *self.get_mut_player_bet(*key)? *= 2.0;
             }
         }
         Ok(())
@@ -236,7 +258,6 @@ impl GameState {
                 let new_card = self.deck.remove(0);
                 self.get_mut_player_hand(player)?.push(new_card);
                 if self.sum_hand(player)? > 21 {
-                    *self.get_mut_player_bet(player)? *= 0;
                     self.action(GameAction::Stand, player).ok();
                 }
                 Ok(vec![ClientEvent::CardRevealed(
@@ -245,12 +266,20 @@ impl GameState {
                 )])
             }
             GameAction::Stand if self.current_player == Some(player) => {
-                let next_player = self.next_current_player(player).ok();
-                if next_player == None {
-                    self.dealer_draw_final();
-                    self.compare_hands().ok();
-                    self.return_bet().ok();
+                // Make sure the next_current_player hand has playable actions, else cycle through player_list, if reaches end of list then end game
+                // Error checking for natural blackjacks
+                self.player_round_over.push(player);
+                while self.player_round_over.contains(&self.get_result_current_player()?){
+                    let next_player = self.next_current_player(self.get_result_current_player()?).ok();
+                    if next_player == None {
+                        ClientEvent::CardRevealed(FromPlayer::Dealer, self.dealer_hand[1]);
+                        self.dealer_draw_final();
+                        self.compare_hands().ok();
+                        self.return_bet().ok();
+                        break;
+                    }
                 }
+
                 Ok(vec![ClientEvent::RoundOver])
             }
             GameAction::Double
@@ -258,23 +287,39 @@ impl GameState {
                     && self.get_player_hand(player)?.len() == 2 =>
             {
                 // First two cards equal to 9, 10, or 11
-                let bet = self.get_player_bet(player)?;
-                if bet <= self.get_player_money(player)? {
-                    *self.get_mut_player_money(player)? -= bet;
-                    *self.get_mut_player_bet(player)? *= 2;
-                    self.action(GameAction::Hit, player).ok();
+                let sum = self.sum_hand(player)?;
+                if sum == 9 || sum == 10 || sum == 11 {
+                    let bet = self.get_player_bet(player)?;
+                    if bet <= self.get_player_money(player)? {
+                        *self.get_mut_player_money(player)? -= bet;
+                        *self.get_mut_player_bet(player)? *= 2.0;
+                        self.action(GameAction::Hit, player).ok();
+                        self.action(GameAction::Stand, player).ok();
+                    }
                 }
                 Ok(vec![ClientEvent::RoundOver])
             }
-            GameAction::AddMoney(value) if value > 0 => {
+            GameAction::AddMoney(value) if value > 0.0 => {
                 *self.get_mut_player_money(player)? += value;
                 Ok(vec![ClientEvent::Betting(player, value)])
             }
-            GameAction::StartingBet(bet) if bet > 0 && bet <= self.get_player_money(player)? => {
+            GameAction::StartingBet(bet) if bet > 0.0 && bet <= self.get_player_money(player)? => {
                 self.player_bet.insert(player, bet);
                 *self.get_mut_player_money(player)? -= bet;
                 if self.player_list.len() == self.player_bet.len() {
                     self.start_game();
+                    self.check_natural_blackjack().ok();
+                    // Make sure the next_current_player hand has playable actions, else cycle through player_list, if reaches end of list then end game
+                    while self.player_round_over.contains(&self.get_result_current_player()?){
+                        let next_player = self.next_current_player(self.get_result_current_player()?).ok();
+                        if next_player == None {
+                            ClientEvent::CardRevealed(FromPlayer::Dealer, self.dealer_hand[1]);
+                            self.dealer_draw_final();
+                            self.compare_hands().ok();
+                            self.return_bet().ok();
+                            break;
+                        }
+                    }
                 }
                 Ok(vec![ClientEvent::Betting(player, bet)])
             }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,11 @@
 #![allow(dead_code)]
 mod gamecoordinator;
 mod gamestate;
-use gamecoordinator::{GameCoordinator};
+use gamecoordinator::GameCoordinator;
 use gamestate::{GameAction, GameState, PlayerID};
 
 fn main() {
-    // Testing
-    let mut test = GameCoordinator::new();
-    test.on_new_user();
-    test.on_new_user();
-    test.on_new_user();
-    test.on_new_user();
-    println!("{:?}", test.get_mut_current_games());
+   todo!();
 }
 
 #[cfg(test)]
@@ -21,126 +15,252 @@ mod tests {
     use gamestate::{ClientEvent, FromPlayer, GameAction, GameError, GameState, PlayerID};
 
     #[test]
-    fn negative_add_money() -> Result<(), GameError> {
+    fn gamecoordinator() -> Result<(), GameError> {
+        // Test GameCoordinator
+        let mut coordinator = GameCoordinator::new();
+
+        // After four players in waiting room, start new game
+        assert_eq!(0, coordinator.get_mut_current_games().len());
+        let player1 = coordinator.on_new_user();
+        coordinator.on_new_user();
+        coordinator.on_new_user();
+        coordinator.on_new_user();
+        assert_eq!(1, coordinator.get_mut_current_games().len());
+
+        // Test GameState
+        coordinator.get_mut_current_games()[0].create_users_hand();
+        coordinator.get_mut_current_games()[0].get_mut_deck().append(&mut vec![11, 9]);
+        
+        assert_eq!(4, coordinator.get_mut_current_games()[0].get_player_list().len());
+        assert_eq!(vec![11, 9], *coordinator.get_mut_current_games()[0].get_deck());
+
+        // Player 2 is in waiting room
+        let player2 = coordinator.on_new_user();
+
+        assert_eq!(true, coordinator.get_mut_current_games()[0].get_player_list().contains(&player1));
+        assert_eq!(true, coordinator.get_available_players().contains(&player2));
+
+        // Test on_dropped_user remove players
+        coordinator.on_dropped_user(player1);
+        coordinator.on_dropped_user(player2);
+
+        assert_eq!(false, coordinator.get_mut_current_games()[0].get_player_list().contains(&player1));
+        assert_eq!(false, coordinator.get_available_players().contains(&player2));
+
+        Ok(())
+    }
+
+    #[test]
+    fn remove_player_game() -> Result<(), GameError> {
+        // Test remove users
         let player1 = PlayerID::new_v4();
-        let mut game: GameState = GameState::new(&vec![player1]);
+        let player2 = PlayerID::new_v4();
+        let mut game: GameState = GameState::new(vec![player1, player2]);
         game.create_users_hand();
-        game.action(GameAction::AddMoney(-100), player1).ok();
-        assert_eq!(0, game.get_player_money(player1)?);
+        assert_eq!(true, game.get_player_list().contains(&player1));
+        game.remove_user(player1);
+        assert_eq!(false, game.get_player_list().contains(&player1));
+        assert_eq!(true, game.get_player_list().contains(&player2));
+        Ok(())
+    }
+
+    #[test]
+    fn negative_add_money() -> Result<(), GameError> {
+        // Cannot use AddMoney for a negative value
+        let player1 = PlayerID::new_v4();
+        let mut game: GameState = GameState::new(vec![player1]);
+        game.create_users_hand();
+        game.action(GameAction::AddMoney(-100.0), player1).ok();
+        assert_eq!(0.0, game.get_player_money(player1)?);
         Ok(())
     }
 
     #[test]
     fn over_bet_money() -> Result<(), GameError> {
+        // Cannot over bet money that you don't have
         let player1 = PlayerID::new_v4();
-        let mut game: GameState = GameState::new(&vec![player1]);
+        let mut game: GameState = GameState::new(vec![player1]);
         game.create_users_hand();
-        game.action(GameAction::AddMoney(100), player1).ok();
-        game.action(GameAction::StartingBet(200), player1).ok();
-        assert_eq!(100, game.get_player_money(player1)?);
+        game.action(GameAction::AddMoney(100.0), player1).ok();
+        game.action(GameAction::StartingBet(200.0), player1).ok();
+        assert_eq!(100.0, game.get_player_money(player1)?);
         Ok(())
     }
 
     #[test]
     fn is_current_player_after_all_bet() -> Result<(), GameError> {
+        // Makes sure the first player in player_list is the current_player after betting
         let player1 = PlayerID::new_v4();
         let player2 = PlayerID::new_v4();
-        let mut game: GameState = GameState::new(&vec![player1, player2]);
+        let mut game: GameState = GameState::new(vec![player1, player2]);
         game.create_users_hand();
-        game.action(GameAction::AddMoney(100), player1).ok();
-        game.action(GameAction::StartingBet(100), player1).ok();
+        game.get_mut_deck().append(&mut vec![11, 10, 2, 10, 10, 8, 10]);
+        game.action(GameAction::AddMoney(100.0), player1).ok();
+        game.action(GameAction::StartingBet(100.0), player1).ok();
         assert_eq!(None, game.get_current_player());
-        game.action(GameAction::AddMoney(100), player2).ok();
-        game.action(GameAction::StartingBet(100), player2).ok();
-        assert_eq!(Some(player1), game.get_current_player());
+        game.action(GameAction::AddMoney(100.0), player2).ok();
+        game.action(GameAction::StartingBet(100.0), player2).ok();
+        assert_eq!(vec![11, 10], *game.get_player_hand(player1)?);
+        assert_eq!(Some(player2), game.get_current_player());
+        game.action(GameAction::Stand, player2).ok();
+        Ok(())
+    }
+
+    #[test]
+    fn natural_blackjack() -> Result<(), GameError> {
+        // Test natural_blackjack action with one player
+        let player1 = PlayerID::new_v4();
+        let mut game: GameState = GameState::new(vec![player1]);
+        game.create_users_hand();
+        game.get_mut_deck().append(&mut vec![11, 2, 10, 10, 6, 3]);
+        game.action(GameAction::AddMoney(100.0), player1).ok();
+        game.action(GameAction::StartingBet(100.0), player1).ok();
+
+        assert_eq!(vec![11, 10], *game.get_player_hand(player1)?);
+        assert_eq!(vec![2, 10, 6], *game.get_dealer_hand());
+        assert_eq!(250.0, game.get_player_money(player1)?);
         Ok(())
     }
 
     #[test]
     fn hit() -> Result<(), GameError> {
+        // Test hit action
         let player1 = PlayerID::new_v4();
-        let mut game: GameState = GameState::new(&vec![player1]);
+        let mut game: GameState = GameState::new(vec![player1]);
         game.create_users_hand();
-        game.action(GameAction::AddMoney(100), player1).ok();
-        game.action(GameAction::StartingBet(100), player1).ok();
-        game.dealer_draw();
-        game.dealer_draw();
-        game.action(GameAction::Hit, player1).ok();
-        game.action(GameAction::Hit, player1).ok();
-        assert_eq!(vec![11, 10], *game.get_player_hand(player1)?);
+        game.get_mut_deck().append(&mut vec![11, 10, 2, 10, 10, 8, 10, 7]);
+        game.action(GameAction::AddMoney(100.0), player1).ok();
+        game.action(GameAction::StartingBet(100.0), player1).ok();
+
+        assert_eq!(vec![11, 2], *game.get_player_hand(player1)?);
+        assert_eq!(vec![10, 10], *game.get_dealer_hand());
         Ok(())
     }
 
     #[test]
     fn stand() -> Result<(), GameError> {
+        // Test stand action
         let player1 = PlayerID::new_v4();
         let player2 = PlayerID::new_v4();
-        let mut game: GameState = GameState::new(&vec![player1, player2]);
+        let mut game: GameState = GameState::new(vec![player1, player2]);
         game.create_users_hand();
-        game.action(GameAction::AddMoney(100), player1).ok();
-        game.action(GameAction::StartingBet(100), player1).ok();
-        game.action(GameAction::AddMoney(100), player2).ok();
-        game.action(GameAction::StartingBet(100), player2).ok();
-        game.dealer_draw();
-        game.dealer_draw();
-        game.action(GameAction::Hit, player1).ok();
-        game.action(GameAction::Hit, player1).ok();
+        game.get_mut_deck().append(&mut vec![11, 10, 2, 10, 10, 8, 10, 7]);
+        game.action(GameAction::AddMoney(100.0), player1).ok();
+        game.action(GameAction::StartingBet(100.0), player1).ok();
+        game.action(GameAction::AddMoney(100.0), player2).ok();
+        game.action(GameAction::StartingBet(100.0), player2).ok();
         game.action(GameAction::Stand, player1).ok();
         assert_eq!(Some(player2), game.get_current_player());
         Ok(())
     }
 
     #[test]
-    fn game() -> Result<(), GameError> {
+    fn double() -> Result<(), GameError> {
+        // Test double action
+        
         let player1 = PlayerID::new_v4();
         let player2 = PlayerID::new_v4();
-        let mut game: GameState = GameState::new(&vec![player1, player2]);
+        let player3 = PlayerID::new_v4();
+        let player4 = PlayerID::new_v4();
+        let mut game: GameState = GameState::new(vec![player1, player2, player3, player4]);
         game.create_users_hand();
-        
-        // Test return value from AddMoney
-        let test_money = game.action(GameAction::AddMoney(100), player1).ok();
-        assert_eq!(test_money, Some(vec![ClientEvent::Betting(player1, 100)]));
+        game.get_mut_deck().append(&mut vec![11, 9, 2, 8, 10, 8, 2, 7, 2, 3, 5, 5, 5, 5]);
 
-        // Test return value from StartingBet
-        let test_bet = game.action(GameAction::StartingBet(100), player1).ok();
-        assert_eq!(test_bet, Some(vec![ClientEvent::Betting(player1, 100)]));
+        game.action(GameAction::AddMoney(100.0), player1).ok();
+        game.action(GameAction::StartingBet(50.0), player1).ok();
+        game.action(GameAction::AddMoney(100.0), player2).ok();
+        game.action(GameAction::StartingBet(75.0), player2).ok();
+        game.action(GameAction::AddMoney(100.0), player3).ok();
+        game.action(GameAction::StartingBet(50.0), player3).ok();
+        game.action(GameAction::AddMoney(100.0), player4).ok();
+        game.action(GameAction::StartingBet(50.0), player4).ok();
 
-        game.action(GameAction::AddMoney(200), player2).ok();
-        game.action(GameAction::StartingBet(50), player2).ok();
+        assert_eq!(vec![11, 8], *game.get_player_hand(player1)?);
 
-        // Test dealer draw and hand
-        game.dealer_draw();
-        game.dealer_draw();
-        assert_eq!(vec![2, 8], *game.get_dealer_hand());
-
-        // Test return value from Hit
-        let test_hit = game.action(GameAction::Hit, player1).ok();
-        assert_eq!(
-            test_hit,
-            Some(vec![ClientEvent::CardRevealed(
-                FromPlayer::Player(player1),
-                11
-            )])
-        );
-        game.action(GameAction::Hit, player1).ok();
-        assert_eq!(vec![11, 10], *game.get_player_hand(player1)?);
+        // Cannot double if hand is not a sum totaling to 9, 10, or 11
+        game.action(GameAction::Double, player1).ok();
+        assert_eq!(50.0, game.get_player_money(player1)?);
+        assert_eq!(50.0, game.get_player_bet(player1)?);
 
         // Test return value from Stand
         let test_stand = game.action(GameAction::Stand, player1).ok();
         assert_eq!(test_stand, Some(vec![ClientEvent::RoundOver]));
 
+        // Cannot double without sufficient money
+        assert_eq!(vec![9, 2], *game.get_player_hand(player2)?);
+        game.action(GameAction::Double, player2).ok();
+        assert_eq!(25.0, game.get_player_money(player2)?);
+        assert_eq!(75.0, game.get_player_bet(player2)?);
+        game.action(GameAction::Stand, player2).ok();
+
+        // Hand sums to 9
+        assert_eq!(vec![2, 7], *game.get_player_hand(player3)?);
+
+        // Doubling is allowed, hitting and standing is automatically done 
+        let test_double_3 = game.action(GameAction::Double, player3).ok();
+        assert_eq!(test_double_3, Some(vec![ClientEvent::RoundOver]));
+        assert_eq!(0.0, game.get_player_money(player3)?);
+        assert_eq!(100.0, game.get_player_bet(player3)?);
+        assert_eq!(vec![2, 7, 5], *game.get_player_hand(player3)?);
+
+        // Doubling with sum of 10
+        assert_eq!(vec![8, 2], *game.get_player_hand(player4)?);
+        game.action(GameAction::Double, player4).ok();
+        assert_eq!(0.0, game.get_player_money(player4)?);
+        assert_eq!(vec![8, 2, 5], *game.get_player_hand(player4)?);
+
+        // After everyone stands, the dealer draws cards until >= 17
+        assert_eq!(vec![10, 3, 5], *game.get_dealer_hand());
+        Ok(())
+    }
+
+    #[test]
+    fn bets_returned() -> Result<(), GameError> {
+        // Tests if bets are returned
+        let player1 = PlayerID::new_v4();
+        let player2 = PlayerID::new_v4();
+        let mut game: GameState = GameState::new(vec![player1, player2]);
+        game.create_users_hand();
+        game.get_mut_deck().append(&mut vec![11, 10, 2, 10, 10, 8, 10, 7]);
+
+        // Test return value from AddMoney
+        let test_money = game.action(GameAction::AddMoney(100.0), player1).ok();
+        assert_eq!(test_money, Some(vec![ClientEvent::Betting(player1, 100.0)]));
+
+        // Test return value from StartingBet
+        let test_bet = game.action(GameAction::StartingBet(100.0), player1).ok();
+        assert_eq!(test_bet, Some(vec![ClientEvent::Betting(player1, 100.0)]));
+
+        game.action(GameAction::AddMoney(200.0), player2).ok();
+
+        // After everyone bets start_game is run, dealing cards
+        game.action(GameAction::StartingBet(50.0), player2).ok();
+
+        // Test dealer hand
+        assert_eq!(vec![2, 8], *game.get_dealer_hand());
+
+        // Test player1 hand (Natural BlackJack returns x2.5)
+        assert_eq!(vec![11, 10], *game.get_player_hand(player1)?);
+        // Bets are mutated at the end of the round 
+        assert_eq!(100.0, game.get_player_bet(player1)?);
+
         // Player hits and busts, ending game since no one is next in player_list
-        game.action(GameAction::Hit, player2).ok();
-        game.action(GameAction::Hit, player2).ok();
-        game.action(GameAction::Hit, player2).ok();
+        let test_hit = game.action(GameAction::Hit, player2).ok();
+        assert_eq!(
+            test_hit,
+            Some(vec![ClientEvent::CardRevealed(
+                FromPlayer::Player(player2),
+                10
+            )])
+        );
         assert_eq!(vec![10, 10, 10], *game.get_player_hand(player2)?);
 
         // After everyone stands, the dealer draws cards until >= 17
         assert_eq!(vec![2, 8, 7], *game.get_dealer_hand());
 
         // Bets returned
-        assert_eq!(200, game.get_player_money(player1)?);
-        assert_eq!(150, game.get_player_money(player2)?);
+        assert_eq!(250.0, game.get_player_money(player1)?);
+        assert_eq!(150.0, game.get_player_money(player2)?);
 
         Ok(())
     }
